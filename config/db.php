6d5d3ef4e398510986df8fe82c666a8054ccc850
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2018-02-16 15:31:20 +0800

    int

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=172.73.249.4;dbname=fcm',
+    'username' => 'fcm',
+    'password' => 'fcm',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
